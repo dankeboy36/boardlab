f60977b75ee3def9c42a823bc08ba8883d149cfa
dankeboy36 <dankeboy36@gmail.com>
2025-12-24 14:50:00 +0100

    fix: ditch deprecated deps
    Signed-off-by: dankeboy36 <dankeboy36@gmail.com>

--- packages/extension/src/test/suite/index.ts
+++ packages/extension/src/test/suite/index.ts
@@ -1,9 +1,9 @@
 import path from 'node:path'
 
-import glob from 'glob'
+import { glob } from 'glob'
 import Mocha from 'mocha'
 
-export function run(): Promise<void> {
+export async function run(): Promise<void> {
   // Create the mocha test
   const mocha = new Mocha({
     ui: 'bdd',
@@ -13,27 +13,15 @@ export function run(): Promise<void> {
 
   const testsRoot = path.resolve(__dirname, '..')
 
-  return new Promise((resolve, reject) => {
-    glob('**/**.test.js', { cwd: testsRoot }, (err, files) => {
-      if (err) {
-        return reject(err)
-      }
+  const files = await glob('**/**.test.js', { cwd: testsRoot })
+  files.forEach((f) => mocha.addFile(path.resolve(testsRoot, f)))
 
-      // Add files to the test suite
-      files.forEach((f) => mocha.addFile(path.resolve(testsRoot, f)))
-
-      try {
-        // Run the mocha test
-        mocha.run((failures) => {
-          if (failures > 0) {
-            reject(new Error(`${failures} tests failed.`))
-          } else {
-            resolve()
-          }
-        })
-      } catch (err) {
-        console.error(err)
-        reject(err)
+  await new Promise((resolve, reject) => {
+    mocha.run((failures) => {
+      if (failures > 0) {
+        reject(new Error(`${failures} tests failed.`))
+      } else {
+        resolve(undefined)
       }
     })
   })
